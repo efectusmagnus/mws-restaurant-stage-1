4e45f2ef43bd48294b5715de77fcf66ae2c5199f
July Natalia da Silva Delgado Habermann <efectusmagnus@gmail.com>
2018-11-12 20:25:21 +0100

    Install Service Worker (test_v1)

--- sw.js
+++ sw.js
@@ -1,3 +1,56 @@
 /**
 * serviceWorker
+* Registration is located in "js/register_sw.js"
 */
+
+/**
+* @describe Name of cache and path of urls to cache.
+*/
+const cacheName = 'restaurant-reviews-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/restaurant.html',
+  '/css/styles.css',
+  '/data/restaurant.json',
+  'js/dbhelper.js',
+  'js/main.js',
+  'js/register_sw.js',
+  'js/restaurant_info.js',
+  'img/1.jpg',
+  'img/2.jpg',
+  'img/3.jpg',
+  'img/4.jpg',
+  'img/5.jpg',
+  'img/6.jpg',
+  'img/7.jpg',
+  'img/8.jpg',
+  'img/9.jpg',
+  'img/10.jpg',
+];
+
+/**
+* @describe Installing serviceWorker.
+*/
+self.addListener('install', e => {
+  console.log('Service Worker has been installed! :)');
+  e.waitUntil(
+    caches.open('cacheName').then('cache' => {
+      console.log('Service Worker is caching the files! :)');
+      return cache.addAll(urlsToCache);
+    }).catch(err => {
+      console.log(`Installation of Service Worker has failed due to ${err}`);
+    })
+  );
+});
+
+/**
+* @describe serviceWorker intercepts request and return cached version.
+*/
+self.addListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
