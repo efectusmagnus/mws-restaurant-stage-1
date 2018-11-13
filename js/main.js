7c96464dcdba22e6fc0408afeee013690a314950
July Natalia da Silva Delgado Habermann <efectusmagnus@gmail.com>
2018-11-13 02:46:02 +0100

    Change position of register.js for loading purposes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+/**
+* @describe Registering a serviceWorker
+*/
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js')
+  .then((reg) => console.log(`Service Worker's Registration was successful! :)`))
+  .catch(err => console.log(`Registration failed due to ${err} :(`));
+}
+/**
+* @describe Main file
+*/
 let restaurants,
   neighborhoods,
   cuisines
--- js/register_sw.js
+++
@@ -1,8 +0,0 @@
-/**
-* @describe Registering a serviceWorker
-*/
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js')
-  .then((reg) => console.log(`Service Worker's Registration was successful! :)`))
-  .catch(err => console.log(`Registration failed due to ${err} :(`));
-}
