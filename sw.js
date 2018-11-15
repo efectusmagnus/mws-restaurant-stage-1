5b1f5354da33025fe567224905b87585f444cf35
July Natalia da Silva Delgado Habermann <efectusmagnus@gmail.com>
2018-11-15 07:25:14 +0100

    Test manifest.json

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ self.addEventListener('install', event => {
           '/restaurant.html?id=8',
           '/restaurant.html?id=9',
           '/restaurant.html?id=10',
-          '/img/logo-efectusmagnus-vocal.png',
+          '/img/Efectusmagnus-white-192.png',
           '/img/owl-offline.jpg'
         ]).catch(error => {
           console.log('Caches open failed: ' + error);
